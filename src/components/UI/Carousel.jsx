5e65b5d41c6f0a09d2369b39ea00b02b43db71cf
davidbirch4272 <davidbirch4272@gmail.com>
2025-06-04 15:17:19 -0400

    Changes

--- src/components/UI/Carousel.jsx
+++ src/components/UI/Carousel.jsx
@@ -3,37 +3,27 @@ import "owl.carousel/dist/assets/owl.theme.default.css";
 import React from "react";
 import OwlCarousel from "react-owl-carousel";
 
-export default function Carousel({
-  children,
-  defaultitemwidth = 535,
-  items = 4,
-  loop = true,
-  margin = 10,
-  autoplay = false,
-  nav = true,
-  dots = false,
-  responsive = { 
-        0: { items: 1 },
-        600: { items: 2 },
-        983: { items: 3 },
-        1198: { items: 4 },
-      }
-      
-}) {
+const Carousel = ({ children }) => {
+  const options = {
+    defaultitemwidth: 535,
+    items: 4,
+    loop: true,
+    margin: 10,
+    autoplay: false,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: { items: 1 },
+      600: { items: 2 },
+      983: { items: 3 },
+      1198: { items: 4 },
+    },
+  };
   return (
-    <OwlCarousel
-      className="owl-theme"
-      loop={loop}
-      margin={margin}
-      nav={nav}
-      items={items}
-      autoplay={autoplay}
-      dots={dots}
-      defaultitemwidth={defaultitemwidth}
-      responsive={responsive}
-      
-    >
+    <OwlCarousel className="owl-theme" {...options}>
       {children}
-          </OwlCarousel>
+    </OwlCarousel>
   );
-}
+};
+
+export default Carousel;
